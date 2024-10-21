c84ebd20a6808cd88542ecb7620bd11352c58be8
waynetheisinger <wayne@mowdirect.co.uk>
2024-10-21 12:17:56 +0100

    dev(routes): move to file

--- VueApp/src/main.js
+++ VueApp/src/main.js
@@ -1,19 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './assets/app.css' // Import the global CSS
-import { createRouter, createWebHistory } from 'vue-router'
-import Method2App from './components/Method2App.vue'
-import PostView from './components/PostView.vue'
-
-const routes = [
-  { path: '/', component: Method2App },
-  { path: '/post/:id', component: PostView }, // Route for individual post view
-]
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes,
-})
+import './assets/app.css'
+import router from './router'
 
 const app = createApp(App)
 app.use(router)
